a77a31d46b8f023c76347ce2131e31dda3905fef
K3bycz <pkapustka@edu.cdv.pl>
2023-11-01 16:14:11 +0100

    Aktualizacja Bootstrapa

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -4,6 +4,8 @@
         <link href="https://fonts.cdnfonts.com/css/public-pixel" rel="stylesheet">
         <link rel="stylesheet" href="{{ asset('css/main.css') }}">
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"></script> -->
         <script src="{{ asset('js/app.js') }}"></script>
 
         <title> @yield('title', $applicationName ) </title>
