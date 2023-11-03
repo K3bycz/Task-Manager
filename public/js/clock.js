967b1c5295ca1e55621a15bc9995191466bd3682
Piotr Kapustka <117202412+K3bycz@users.noreply.github.com>
2023-11-03 13:01:21 +0100

    Revert "Project-BootstrapUpgrade"

--- public/js/clock.js
+++ public/js/clock.js
@@ -1,9 +1,10 @@
-    
+document.addEventListener('DOMContentLoaded', function() {
     function clock() {
         const now = new Date();
         const options = { timeZone: 'Europe/Warsaw', hour: 'numeric', minute: 'numeric', second: 'numeric' };
         const formattedTime = now.toLocaleTimeString('pl-PL', options);
-        $('#clock').text(formattedTime);
+        document.getElementById('clock').textContent = formattedTime;
     }
 
     setInterval(clock, 1000);
+});
--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -2,14 +2,11 @@
     <head>
         <meta charset="utf-8">
         <title> @yield('title', $applicationName ) </title>
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
         <link href="https://fonts.cdnfonts.com/css/public-pixel" rel="stylesheet">
         <link rel="stylesheet" href="{{ asset('css/main.css') }}">
-        
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"></script>
-
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"></script> -->
         <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
         <script src="{{ asset('js/clock.min.js') }}"></script>
 
@@ -17,31 +14,30 @@
             <div>
                 <a href="/user"> <img src="{{ asset('/images/user.png') }}" alt="User" style="width:40px;height:40px;right: 1%;position: fixed; padding-top:5px;"></a>
             </div>
-            <div style="display: flex; justify-content: center;padding-top:10px;font-size: 24px;font-family:Comic Sans MS, Comic Sans, cursive;">
+            <div style="  display: flex; justify-content: center;padding-top:10px;font-size: 24px;font-family:Comic Sans MS, Comic Sans, cursive;">
                 @yield('title', $applicationName) 
             </div>
         </div>
     </head>
-
     <body>
-        <div class="sidebar"> 
+        <div class="sidebar">   
             @section('sidebar')
-                <ul class="list-unstyled">
+                <ul>
                     <li><a href="/" >&#128726; Pulpit</a></li>
                 </ul>
-                <ul class="list-unstyled">
+                <ul>
                     <li><br>ZADANIA</li>
                 </ul>
-                <ul class="list-unstyled">
+                <ul>
                     <li><a href="/tasks" >&#x1F5F9; Lista</a></li>
                 </ul>
-                <ul class="list-unstyled">
+                <ul>
                     <li><a href="/tasks/create" >&#10133; Dodaj</a></li>
                 </ul>
-                <ul class="list-unstyled">
+                <ul>
                     <li><br>UŻYTKOWNICY</li>
                 </ul>
-                <ul class="list-unstyled">
+                <ul>
                     <li><a href="/users" >&#128100; Lista</a></li>
                 </ul>
             @show
