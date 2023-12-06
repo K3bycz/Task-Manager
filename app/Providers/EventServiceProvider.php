af962ad5f1bf87ac21bfdb0493e08d204729f25d
K3bycz <pkapustka@edu.cdv.pl>
2023-12-06 13:00:29 +0100

    Poprawa błędu z dublującymi się mailami w bazie

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -20,9 +20,6 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
-        MessageSent::class => [
-            SaveSentEmail::class,
-        ],
     ];
 
     /**
